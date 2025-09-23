4f4a4f3b90ac5eba9cb5d78fd359960a0800ce4a
Ryan CY <phillin820109@msn.com>
2025-09-23 10:54:49 +0800

    feat(db): add Events and Participants tables

--- database/migrations/2025_09_22_145903_create_events_table.php
+++ database/migrations/2025_09_22_145903_create_events_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -13,6 +14,16 @@ return new class extends Migration
     {
         Schema::create('events', function (Blueprint $table) {
             $table->id();
+
+            $table->foreignIdFor(User::class);
+            $table->string('name');
+            $table->text('description')->nullable();
+            $table->string('location');
+            $table->unsignedTinyInteger('capacity')->nullable();
+
+            $table->dateTime('start');
+            $table->dateTime('end');
+            
             $table->timestamps();
         });
     }
