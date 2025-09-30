c9a6ff871d54c3b58995a307638b5ea16c4ea708
Ryan CY <phillin820109@msn.com>
2025-09-30 22:56:45 +0800

    feat(api): add list participants endpoint

--- app/Http/Controllers/Api/ParticipantController.php
+++ app/Http/Controllers/Api/ParticipantController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\ParticipantResource;
+use App\Models\Event;
 use Illuminate\Http\Request;
 
 class ParticipantController extends Controller
@@ -10,9 +12,11 @@ class ParticipantController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Event $event)
     {
-        //
+        $participants = $event->participants()->latest()->paginate();
+        
+        return ParticipantResource::collection($participants);
     }
 
     /**
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\EventController;
+use App\Http\Controllers\Api\ParticipantController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -9,3 +10,5 @@ Route::get('/user', function (Request $request) {
 })->middleware('auth:sanctum');
 
 Route::apiResource('events', EventController::class);
+
+Route::apiResource('events.participants', ParticipantController::class)->scoped();
