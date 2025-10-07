18d4387b6f9f0efe2ddb9635c97c8e8a990f2fa9
Ryan CY <phillin820109@msn.com>
2025-10-07 10:49:56 +0800

    feat(auth): add user login API with token authentication

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\EventController;
 use App\Http\Controllers\Api\ParticipantController;
 use Illuminate\Http\Request;
@@ -8,6 +9,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::post('/login', [AuthController::class, 'login']);
 
 Route::apiResource('events', EventController::class);
 
