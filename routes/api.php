e7791c56dc61f4b67edfec4e3cf4e4cf5f16b0e5
Ryan CY <phillin820109@msn.com>
2025-10-07 14:50:51 +0800

    feat(auth): add user logout API with token authentication

--- routes/api.php
+++ routes/api.php
@@ -6,12 +6,12 @@ use App\Http\Controllers\Api\ParticipantController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware('auth:sanctum')->group(function() {
+    Route::get('/user', fn(Request $request) => $request->user());
+    Route::post('/logout', [AuthController::class, 'logout']);
 });
 
 Route::post('/login', [AuthController::class, 'login']);
 
 Route::apiResource('events', EventController::class);
-
 Route::apiResource('events.participants', ParticipantController::class)->scoped()->except('update');
