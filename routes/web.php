13775575f5d73a11df202d9f8b74b1c595c2390b
Ryan CY <phillin820109@msn.com>
2025-10-08 15:55:14 +0800

    feat(web): add routes for Event and Participant controllers

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Http\Controllers\Web\EventController as WebEventController;
+use App\Http\Controllers\Web\ParticipantController as WebParticipantController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('events', WebEventController::class);
+Route::resource('events.participants', WebParticipantController::class)->scoped()->except('update');
